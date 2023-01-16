ec1e3a07d190b2508037ea631553ac523108ec74
susanahmed <susanahmed617@gmail.com>
2023-01-15 19:38:20 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// code your solution here
+function superbowlWin(records) {
+    const wins = records.find(function(record) {
+    return (record.result === 'W')
+    
+})
+    if (wins) {
+        return wins.year;
+    }
+    else {
+        return undefined
+    }
+}
